3adc7e24eeaaaef055db51c3b7a31e7e164dface
Anuj Dubey <anujd2408@gmail.com>
2025-09-15 19:06:28 +0530

    added vercel.json

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -15,34 +15,7 @@ const config: Config = {
         gw : "#758ba5",
         bw : "#d1dded"
       },
-       animation: {
-        typewriter: 'typewriter 2s steps(11) forwards',
-        caret: 'typewriter 2s steps(69) forwards, blink 1s steps(69) infinite 2s',
-      },
-      keyframes: {
-        typewriter: {
-          to: {
-            left: '100%',
-          },
-        },
-        blink: {
-          '0%': {
-            opacity: '0',
-          },
-          '0.1%': {
-            opacity: '1',
-          },
-          '50%': {
-            opacity: '1',
-          },
-          '50.1%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '0',
-          },
-        },
-      },
+       
     },
   },
   plugins: [],
